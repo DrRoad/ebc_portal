c0dfe1dedc3b5ee17f31c30c5c7c7741e1d6dea7
timelyportfolio <kent.russell@timelyportfolio.com>
2017-07-08 11:37:41 -0500

    test 9 iphone only min-width

--- ebc_vue/src/App.vue
+++ ebc_vue/src/App.vue
@@ -24,10 +24,10 @@
         </div>
 
       </div>
-      <div class="row align-items-center justify-content-end hidden-md-up">>
+      <div class="row align-items-center justify-content-end hidden-md-up">
           <a href="http://www.snappartnership.net/">
             <svg viewBox="0,0,290,75"
-                 style="max-width:200px; min-width:90px;background-color: rgb(131,146,143);">
+                 style="min-width:150px; background-color: rgb(131,146,143);">
               <image xlink:href="./assets/snap-acronym-color-white.png" x="0" y="0" height="75px" width="290px"/>
             </svg>
           </a>
