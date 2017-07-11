fd1e10cb31419c52df01037dd72feaf81484f414
timelyportfolio <kent.russell@timelyportfolio.com>
2017-07-10 19:49:10 -0500

    bump width on about tool data

--- ebc_vue/src/components/AboutTool.vue
+++ ebc_vue/src/components/AboutTool.vue
@@ -2,7 +2,7 @@
 
   <div class="container" style="margin-top: 2em;">
     <div class="row align-items-start justify-content-center">
-      <div class="col-8">
+      <div class="col col-9">
         <div style="color:#006699">
           <h2 align="center">
             <strong>ABOUT THE DATA &amp; TOOL</strong>
@@ -12,7 +12,7 @@
     </div>
 
     <div class="row align-items-start justify-content-center">
-      <div class="col-8">
+      <div class="col col-9">
         <ul class="nav nav-tabs" role="tablist">
           <li class="nav-item">
             <router-link class="nav-link" to="/about/tool/data" active-class="active" role="tab">The Data</router-link>
@@ -29,7 +29,7 @@
 
     <div class = "tab-content">
       <div class="row align-items-start justify-content-center">
-        <div class="col col-8">
+        <div class="col col-9">
           <router-view></router-view>
         </div>
       </div>
