6bc6a8405d9bd82e45ac60ccc89b2b43ffe21f61
timelyportfolio <kent.russell@timelyportfolio.com>
2017-07-24 19:14:48 -0500

    add collapse in filter poc

--- ebc_vue/src/components/Filters.vue
+++ ebc_vue/src/components/Filters.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
-    <h3>Filters</h3>
-    <h4>Geographic</h4>
-    <tree
-      :data="geo" :props="defaultProps"
-      :default-checked-keys="geo.children.map(d=>d.name)"
-      node-key="name"
-      show-checkbox
-      :expand-on-click-node = "false"
-      @check-change="handleCheckChange" @node-click="handleNodeClick"
-    >
-    </tree>
-    <h4>Habitat</h4>
-    <h4>Intervention</h4>
-    <h4>Outcome</h4>
+    <h4>Filters</h4>
+
+    <h5>
+      <a data-toggle="collapse" href="#collapseGeo" aria-expanded="true" aria-controls="collapseGeo">Geographic</a>
+    </h5>
+    <div class="collapse" id="collapseGeo">
+      <tree
+        :data="geo" :props="defaultProps"
+        :default-checked-keys="geo.map(d=>d.name)"
+        node-key="name"
+        show-checkbox
+        :expand-on-click-node = "false"
+        @check-change="handleCheckChange" @node-click="handleNodeClick"
+      >
+      </tree>
+    </div>
+
+    <h5>
+    <a data-toggle="collapse" href="#collapseHabitat" aria-expanded="true" aria-controls="collapseHabitat">Habitat</a>
+    <div class="collapse" id="collapseHabitat">
+      habitat filters
+    </div>
+    </h5>
+
+    <h5>
+    <a data-toggle="collapse" href="#collapseIntervention" aria-expanded="true" aria-controls="collapseIntervention">Intervention</a>
+    <div class="collapse" id="collapseIntervention">
+      intervention filters
+    </div>
+    </h5>
+    
+    <h5>
+    <a data-toggle="collapse" href="#collapseOutcome" aria-expanded="true" aria-controls="collapseOutcome">Outcome</a>
+    <div class="collapse" id="collapseOutcome">
+      outcome filters
+    </div>
+    </h5>
   </div>
 </template>
 
@@ -27,7 +50,7 @@ export default {
   },
   data() {
     return {
-      geo: Geofilters(),
+      geo: Geofilters().children,
       defaultProps: {
         children: 'children',
         label: 'name'
