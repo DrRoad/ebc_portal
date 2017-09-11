4058f000f1a91d2e9ccb85e0adc546ef9b6bf021
timelyportfolio <kent.russell@timelyportfolio.com>
2017-09-11 06:41:36 -0500

    add zoom drag on geo

--- ebc_vue/src/components/Explore.vue
+++ ebc_vue/src/components/Explore.vue
@@ -233,7 +233,7 @@ export default {
       return {        
         "$schema": "https://vega.github.io/schema/vega/v3.0.json",
         "width": 600,
-        "height": 400,
+        "height": 500,
         "autosize": "none",
 
         "encode": {
@@ -243,14 +243,68 @@ export default {
         },
 
         "signals": [
-          { "name": "scale", "value": 90 },
-          { "name": "rotate0", "value": -12 },
-          { "name": "rotate1", "value": 0 },
-          { "name": "rotate2", "value": 0 },
-          { "name": "center0", "value": 0 },
-          { "name": "center1", "value": 0 },
           { "name": "tx", "update": "width / 2"},
           { "name": "ty", "update": "height / 2"},
+          {
+            "name": "scale",
+            "value": 150,
+            "on": [{
+              "events": {"type": "wheel", "consume": true},
+              "update": "clamp(scale * pow(1.0005, -event.deltaY * pow(16, event.deltaMode)), 150, 3000)"
+            }]
+          },
+          {
+            "name": "angles",
+            "value": [0, 0],
+            "on": [{
+              "events": "mousedown",
+              "update": "[rotateX, centerY]"
+            }]
+          },
+          {
+            "name": "cloned",
+            "value": null,
+            "on": [{
+              "events": "mousedown",
+              "update": "copy('projection')"
+            }]
+          },
+          {
+            "name": "start",
+            "value": null,
+            "on": [{
+              "events": "mousedown",
+              "update": "invert(cloned, xy())"
+            }]
+          },
+          {
+            "name": "drag", "value": null,
+            "on": [{
+              "events": "[mousedown, window:mouseup] > window:mousemove",
+              "update": "invert(cloned, xy())"
+            }]
+          },
+          {
+            "name": "delta", "value": null,
+            "on": [{
+              "events": {"signal": "drag"},
+              "update": "[drag[0] - start[0], start[1] - drag[1]]"
+            }]
+          },
+          {
+            "name": "rotateX", "value": 0,
+            "on": [{
+              "events": {"signal": "delta"},
+              "update": "angles[0] + delta[0]"
+            }]
+          },
+          {
+            "name": "centerY", "value": 0,
+            "on": [{
+              "events": {"signal": "delta"},
+              "update": "clamp(angles[1] + delta[1], -60, 60)"
+            }]
+          },
 
           { "name": "graticuleDash", "value": 0 },
           { "name": "borderWidth", "value": 1 },
@@ -263,11 +317,8 @@ export default {
             "name": "projection",
             "type": "mercator",
             "scale": {"signal": "scale"},
-            "rotate": [
-              {"signal": "rotate0"},
-              {"signal": "rotate1"},
-              {"signal": "rotate2"}
-            ],
+            "rotate": [{"signal": "rotateX"}, 0, 0],
+            "center": [0, {"signal": "centerY"}],
             "translate": [{"signal": "tx"}, {"signal": "ty"}]
           }
         ],
