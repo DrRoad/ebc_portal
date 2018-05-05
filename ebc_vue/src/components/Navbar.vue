ff66a8ae1f2c27961ec5c4c592ef3aab3a7f54ce
timelyportfolio <kent.russell@timelyportfolio.com>
2018-05-05 08:08:45 -0500

    add background to interactive navbar

--- ebc_vue/src/components/Navbar.vue
+++ ebc_vue/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-md" style="border-bottom:#eee 1px solid;">
+  <nav class="navbar navbar-expand-md">
     <ul class="nav">
       <li class="nav-item">
         <router-link class="nav-link" active-class="active" to="charts">Visualization</router-link>
@@ -16,3 +16,16 @@
     </ul>
   </nav>
 </template>
+
+<style scoped>
+  .navbar {
+    background-color: #ddd;
+    margin-top: 4px;
+    margin-left: -10px;
+    margin-right: -10px;
+  }
+
+  .nav-link.active {
+    border-bottom: 1px #bbb solid;
+  }
+</style>
